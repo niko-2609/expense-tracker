785b731ae553fec3961d54a2acc7ab2f02184db4
nikolai <niko.mak26@gmail.com>
2024-08-07 10:44:21 +0530

    added sign-up form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import RegisterForm from "@/components/auth/register-form";
 
-function SignUp() {
+export default function SignUp() {
   return (
-    <div>
-      
-    </div>
+    <CardWrapper
+      headerLabel="Enter your information to create an account "
+      backButtonHref="/sign-in"
+      backButtonLabel="Already have an account? Sign In"
+      showSocial={true}
+      title="Register"
+    >
+      <RegisterForm />
+    </CardWrapper>
   )
 }
-
-export default SignUp
