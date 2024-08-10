27e9e52f4e8c9c98fd4f819fe5de0b769f47f5b3
nikolai <niko.mak26@gmail.com>
2024-08-10 14:24:00 +0530

    changed overall layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,9 @@ function AppLayout({children}:{children: React.ReactNode}) {
        <Sidebar/>
        <div className="flex flex-col">
        <Header />
+       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
        {children}
+       </main>
        </div>
     </div>
   )
